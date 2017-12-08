9cf504c91732fc8b440324e588f2f5f0df3e8b6f
mikesmall <michael.l.a.small@gmail.com>
2017-12-07 23:03:53 -0500

    Optional shorthand variable declared in the render method (for showing comments)

--- src/App.js
+++ src/App.js
@@ -8,13 +8,20 @@ import Comment from './Comment.js';
 
 class Post extends Component {
   render() {
+    let allComments = [
+      <Comment body={this.props.comments[0]} />,
+      <Comment body={this.props.comments[1]} />,
+      <Comment body={this.props.comments[2]} />,
+      <Comment body={this.props.comments[3]} />,
+      <Comment body={this.props.comments[4]} />,
+    ]
     return (
       <div class="post">
         <h1>{this.props.title}</h1>
-        <h2>By: {this.props.author}</h2>
+        <h2>by {this.props.author}</h2>
         <p>{this.props.body}</p>
         <h3>Comments:</h3>
-        <Comment body={this.props.comments} />
+        {allComments}
       </div>
 
     );
