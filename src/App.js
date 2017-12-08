89161b6307754177c09786f1ca75e2ebf150ee3a
mikesmall <michael.l.a.small@gmail.com>
2017-12-08 14:20:03 -0500

    changeBody method added to Post

--- src/App.js
+++ src/App.js
@@ -8,6 +8,20 @@ import Author from './Author.js';
 // Note: Normally, it would be good practice to rename the App.js file Post.js or to create a new component file for the Post component entirely. Then it would be obvious which component each file contains. However, don't do this! This is a comment for you to know best practices for the future - in this tutorial, leave the file named App.js. We're just concentrating on making it work for now!
 
 class Post extends Component {
+
+  constructor (props) {
+    super()
+    this.state = {
+      body: props.body
+    }
+  }
+  changeBody(e) {
+    let newBody = prompt("What should the new body be?")
+    this.setState({
+      body: newBody
+    })
+  }
+
   render() {
     let allComments = [
       <Comment body={this.props.comments[0]} />,
@@ -25,7 +39,8 @@ class Post extends Component {
       <div class="post">
         <h1>{this.props.title}</h1>
         <h2>{authors}</h2>
-        <p>{this.props.body}</p>
+        <p>{this.state.body}</p>
+        <button onClick={(e) => this.changeBody(e)}>Edit Body</button>
         <div class="comments">
           <h3>Comments:</h3>
           <p>{allComments}</p>
