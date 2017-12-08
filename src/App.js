45eaab98bd5302d56dfe9ed7b8b556415a34ced3
mikesmall <michael.l.a.small@gmail.com>
2017-12-07 20:17:06 -0500

    Creates and renders 'post' object

--- src/App.js
+++ src/App.js
@@ -2,20 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+// Note: Normally, it would be good practice to rename the App.js file Post.js or to create a new component file for the Post component entirely. Then it would be obvious which component each file contains. However, don't do this! This is a comment for you to know best practices for the future - in this tutorial, leave the file named App.js. We're just concentrating on making it work for now!
+
+class Post extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class="post">
+        <h1>{this.props.title}</h1>
+        <h2>By: {this.props.author}</h2>
+        <p>{this.props.body}</p>
+        Comments:
+        <ol>
+          <li>{this.props.comments[0]}</li>
+          <li>{this.props.comments[1]}</li>
+          <li>{this.props.comments[2]}</li>
+          <li>{this.props.comments[3]}</li>
+          <li>{this.props.comments[4]}</li>
+        </ol>
       </div>
+
     );
   }
 }
 
-export default App;
+export default Post;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Post from './App.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+var post = {
+  postTitle: "Dinosaurs Are Awesome",
+  postAuthor: "Stealthy Stegasaurus",
+  postBody: "Thou misbegotten milk-livered bladder! You are as rheumatic as two dry toasts. Out of my sight! Thou dost infect my eyes. Thou fobbing unchin-snouted devil-mon! Thou cullionly spur-galled puttock! Thou pribbling rough-hewn horn-beast! Thou dankish onion-eyed clotpole! Thou dankish onion-eyed clotpole! Thou paunchy dread-bolted giglet! Thou currish common-kissing boar-pig! Thou churlish flap-mouthed fustilarian!",
+  postComments: [
+    "First comment!",
+    "Second comment!",
+    "Third comment!",
+    "Fourth comment!",
+    "Fifth comment!"
+  ]
+}
+
+ReactDOM.render(
+  <Post
+    title={post.postTitle}
+    author={post.postAuthor}
+    body={post.postBody}
+    comments={post.postComments}
+  />,
+document.getElementById('root'));
